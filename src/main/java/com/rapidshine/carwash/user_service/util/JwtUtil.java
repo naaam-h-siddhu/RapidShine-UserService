af69fc595ea5f42433c7cfa7c2948e1c097bd39d
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-04-12 01:32:59 +0530

    Add. configured the centralized config-server

--- src/main/java/com/rapidshine/carwash/user_service/util/JwtUtil.java
+++ src/main/java/com/rapidshine/carwash/user_service/util/JwtUtil.java
@@ -26,7 +26,7 @@ public class JwtUtil {
     public String generateToken(String email, String role) {
         return Jwts.builder()
                 .setSubject(email)
-                .claim("role", role)
+                .claim("role", "ROLE_"+role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(secretKey)
